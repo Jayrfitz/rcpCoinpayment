f55a2497c17cfff4940b8a4edebc9d32b8fb39ff
jayrfitz <jfitzgerald@csumb.edu>
2018-04-09 19:15:09 +0000

    copying some stripe gateways and some coinpayments python

---
+++ wp-content/plugins/restrict-content-pro/includes/gateways/coinpayments/functions.php
@@ -0,0 +1,26 @@
+
+
+<!---->python
+<!--from CryptoPayments import CryptoPayments-->
+
+<!--API_KEY     = 'You Public API Key'-->
+<!--API_SECRET  = 'You API Secret'-->
+<!--IPN_URL = 'Your Callback URL'-->
+
+<!--## Parameters for your call, these are defined in the CoinPayments API Docs-->
+<!--## https://www.coinpayments.net/apidoc-->
+<!--post_params = {-->
+<!--    'amount' : 10,-->
+<!--    'currency1' : 'USD',-->
+<!--    'currency2' : 'LTCT'-->
+<!--}-->
+
+<!--client = CryptoPayments(API_KEY, API_SECRET, IPN_URL)-->
+
+<!--transaction = client.createTransaction(post_params)-->
+
+<!--print transaction-->
+<!--#Prints out transaction details-->
+
+<!--print transaction.amount-->
+<!--print transaction.address-->
